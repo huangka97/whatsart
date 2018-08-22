6e2297398b6ccd9b78cab54dc97b45806ac6cc44
Karl Huang <karl.huang2009@gmail.com>
2018-08-22 14:54:01 -0700

    set up Google Vision Api

---
+++ server.js
@@ -0,0 +1,18 @@
+// Imports the Google Cloud client library
+const vision = require('@google-cloud/vision');
+
+// Creates a client
+const client = new vision.ImageAnnotatorClient();
+
+// Performs label detection on the image file
+client
+  .labelDetection('./resources/demo-image.jpg')
+  .then(results => {
+    const labels = results[0].labelAnnotations;
+
+    console.log('Labels:');
+    labels.forEach(label => console.log(label.description));
+  })
+  .catch(err => {
+    console.error('ERROR:', err);
+  });
