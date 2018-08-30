147a0d3a6334eab5f76aad1f68c357654ae3d70b
kevinnguyen125 <kevinnguyen125@gmail.com>
2018-08-30 13:56:16 -0700

    Dedicated Backend route to attain Wikipedia information for a painting name

---
+++ server/helpers/wikiparse.js
@@ -0,0 +1,80 @@
+import infobox from 'wiki-infobox';
+import crypto from 'crypto';
+
+// Helper: Hash Function for Wikipedia Image Link
+const md5Hash = (fileName) => {
+  const hash = crypto.createHash('md5');
+  hash.update(fileName);
+  return hash.digest('hex');
+};
+
+// Helper: Get Wikipedia Image URL with md5 Hashing
+const getImageUrl = (imageFileName) => {
+  const underscoreName = imageFileName.split(" ").join("_");
+  const encodedURI = encodeURIComponent(underscoreName);
+  const firstTwo = md5Hash(underscoreName).slice(0,2);
+  const url = 'https://upload.wikimedia.org/wikipedia/commons/thumb/' + firstTwo[0]
+              + '/' + firstTwo + '/' + encodedURI + '/' + '500px-' + encodedURI;
+  return url;
+}
+
+// Helper: Get City of Artwork
+const getCity = (cityData) => {
+  if (Object.prototype.toString.call(cityData) === '[object Object]') {
+    return cityData.text || cityData.value
+  }
+  return cityData.map((obj) => obj.text || obj.value).join('');
+}
+
+// Helper: Get Museum of Artwork
+const getMuseum = (museumData) => {
+  if (museumData.constructor === Array) {
+    return museumData[0].text || museumData[0].value;
+  }
+  return museumData.text || museumData.value;
+}
+
+// Helper: Get Medium of Artwork
+const getMedium = (data) => {
+  if (data.medium) {
+    return data.medium.text || data.medium.value;
+  }
+  else if (data.material) {
+    return data.material.map((obj) => obj.text || obj.value).join('');
+  }
+  else if (data.type) {
+    const unformatted = data.type.map((obj) => (obj.text ? obj.text.trim() : '') || (obj.value ? obj.value.trim() : ''));
+    const andIndex = unformatted.indexOf('and');
+    return unformatted.slice(0, andIndex + 1).join(', ') + ' ' + unformatted.slice(andIndex + 1).join(' ');
+  }
+}
+
+// MAIN FUNCTION: Get Wikipedia Information for an Artwork
+const getWikiInfo = (page) => {
+  return new Promise((resolve, reject) => {
+  infobox(page, 'en', function(err, data){
+    if (err) {
+      return reject(`Server could not find wikipedia page for '${page}'`);
+    }
+      console.log(data);
+      try {
+        const parsedData = {
+          title: data.title.value,
+          artist: data.artist.text,
+          year: data.year.value.replace(/{{circa}}/g, 'c.'),
+          city: getCity(data.city),
+          museum: getMuseum(data.museum),
+          imageUrl: getImageUrl(data.image_file.value),
+          medium: getMedium(data),
+        };
+        return resolve(parsedData);
+      }
+      catch(error) {
+        console.log('Server encountered an error:', error);
+        return reject('Server encountered an error: ' + error);
+      }
+    });
+  });
+};
+
+export default getWikiInfo;
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -4,56 +4,33 @@ import mongoose from 'mongoose';
 import restify from 'express-restify-mongoose';
 import { User, Artwork } from '../models/models.js';
 
-// To Parse Wikipedia Information
-import infobox from 'wiki-infobox';
-import crypto from 'crypto';
+// Wikipedia Parsing Helper Function
+import getWikiInfo from '../helpers/wikiparse.js';
 
+// Router & Routes
 const router = express.Router();
 
-// Makes sure that user is logged in
-router.use('/', (req, res, next) => {
-  if (req.user) {
-    next();
-  } else {
-    res.status(404).send();
-  }
-});
+// Checks that User is Logged In Before Accessing Other Routes
+// router.use('/', (req, res, next) => {
+//   if (req.user) {
+//     next();
+//   } else {
+//     res.status(404).send("User is not logged in.");
+//   }
+// });
 
+// RESTIFY API for Accessing Artworks
 restify.serve(router, Artwork);
 
-// Hash Function for Wikipedia Image Link
-const md5Hash = (fileName) => {
-  const hash = crypto.createHash('md5');
-  hash.update(fileName);
-  return hash.digest('hex');
-};
-
-const page = 'The Starry Night';
-
-infobox(page, 'en', function(err, data){
-  if (err) {
-    console.log(`Server could not find wikipedia page for '${page}'`);
-    return;
-  }
-  //console.log(data);
-  const parsedData = {
-    title: data.title.value,
-    artist: data.artist.text,
-    year: data.year.value.replace(/{{circa}}/g, 'c.'),
-    city: data.city.text,
-    museum: data.museum[0].text
-  };
-  //console.log(parsedData);
-  const underscoreName = data.image_file.value.split(" ").join("_");
-  const encodedURI = encodeURIComponent(underscoreName);
-  const firstTwo = md5Hash(underscoreName).slice(0,2);
-  const url = 'https://upload.wikimedia.org/wikipedia/commons/thumb/' + firstTwo[0]
-              + '/' + firstTwo + '/' + encodedURI + '/' + '500px-' + encodedURI;
-  console.log(url);
-});
-
+// POST route for new Artworks for a user
 router.post('/artwork', (req, res, next) => {
-
+  if (!req.body.artworkName) {
+    return res.status(404).send('No Artwork Name Provided.');
+  } else {
+    getWikiInfo(req.body.artworkName)
+    .then((data) => res.json({ success: true, artworkData: data }))
+    .catch((err) => res.json({ success: false, error: err}));
+  }
 });
 
 export default router;
